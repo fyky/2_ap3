b13470dbc69958a1e3059fa6fa3723a3001b6ad7
fyky <fuyu12dmb25ms@gmail.com>
2021-11-08 23:30:59 +0900

    [Start]構築

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @book = Book.new
+    @users = User.all
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render :edit
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
