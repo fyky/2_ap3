cc4393b8355baad6ab9709540cca2b3fb0c1bd63
fyky <fuyu12dmb25ms@gmail.com>
2021-11-09 14:16:08 +0000

    [Add]必要ファイルアップロード

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'homes#index'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :users
+  resources :books, only: [:new, :create, :index, :show, :destroy, :update, :edit]
+end
